540a75e8c43ce863a8c69d4ce062f3c09146bce6
irala <nineirm@gmail.com>
2019-09-06 19:20:01 +0200

    add classic.cpp

---
+++ classic.cpp
@@ -0,0 +1,15 @@
+class classic
+{
+private:
+    /* data */
+    //variables y funciones miembro
+    int variable;
+    int miembro_f();
+    //no puede tener metodos puros (declarados .h),si metodos definido(con cuerpo  .cpp)
+public:
+    classic(/* args */);
+    ~classic();
+    virtual int miembro_f()
+    {
+    }
+};
---
+++ interface.h
@@ -0,0 +1,8 @@
+class interface
+{
+    //no tiene estados
+public:
+    //interfaces no tienen que tener constructor ni destructor
+    //son publicas porque tienen que ser accesibles
+    virtual int miembro_f() = 0;
+};
